791cf0d05a54c7dce85fc05ca3df2e950717f7d1
Susana Becerra Pino <susanabp1992@gmail.com>
2022-01-05 01:47:35 -0500

    On Register.jsx: Dropdown fixed and form validation added

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -77,6 +77,33 @@ const Register = () => {
         let province = provinces[e];
         setProvince(province);
     }
+
+    validateField = (fieldName, value) => {
+        let fieldValidationErrors = this.state.formErrors;
+        let emailValid = this.state.emailValid;
+        let passwordValid = this.state.passwordValid;
+      
+        switch(fieldName) {
+          case 'email':
+            emailValid = value.match(/^([\w.%+-]+)@([\w-]+\.)+([\w]{2,})$/i);
+            fieldValidationErrors.email = emailValid ? '' : ' is invalid';
+            break;
+          case 'password':
+            passwordValid = value.length >= 6;
+            fieldValidationErrors.password = passwordValid ? '': ' is too short';
+            break;
+          default:
+            break;
+        }
+        this.setState({formErrors: fieldValidationErrors,
+                        emailValid: emailValid,
+                        passwordValid: passwordValid
+                      }, this.validateForm);
+      }
+      
+      validateForm = () => {
+        this.setState({formValid: this.state.emailValid && this.state.passwordValid});
+      }
     return (
         <Container>
             <form className='container' onSubmit={handleSubmit}>
